d72d4550f7c715caa887fa7d56ada11f14df75e2
Olivvvvvka <grib0419@mail.ru>
2025-06-09 03:22:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 model = tf.keras.models.load_model("models/car_classification_model.keras")
 scaler = joblib.load("models/scaler.pkl")
 class_encoder = joblib.load("models/class_encoder.pkl")
