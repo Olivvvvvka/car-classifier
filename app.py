c6deb760ae1739d62bf9b17216176eeed6635082
Olivvvvvka <grib0419@mail.ru>
2025-06-09 03:25:17 +0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+from flask import Flask, request, render_template_string
+import numpy as np
+import pandas as pd
 import tensorflow as tf
-import tensorflow as tf
+import joblib
+import os
+
 model = tf.keras.models.load_model("models/car_classification_model.keras")
 scaler = joblib.load("models/scaler.pkl")
 class_encoder = joblib.load("models/class_encoder.pkl")
